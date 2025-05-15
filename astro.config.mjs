6f500d40ca921465195ef2af1e24b2f5a2cecd87
Kaitlyn Barbour <kbarbou2@nd.edu>
2025-05-15 13:48:06 -0400

    Revert "update site and base config for root"
    This reverts commit 6c99f7ca65509982f22862bc314bb479c44449e6.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://kabarbour.github.io',
-  base: '/',
+  site: 'https://kabarbour.github.io/personal', // ✅ must match your GitHub Pages URL
+  base: '/personal/',                           // ✅ must match your repo name
   integrations: [mdx(), sitemap(), tailwind()],
 });
